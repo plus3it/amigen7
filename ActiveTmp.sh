7127e62fd2734ee6bf1204b3cb5651ef123ff44a
Thomas H Jones II <github@xanthia.com>
2016-03-10 16:42:08 +0000

    Ensure /tmp is from tmpfs

---
+++ ActiveTmp.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# Script to ensure that host has /tmp as tmpfs
+#
+#################################################################
+SVCNAM="tmp.mount"
+TMPSVC=$(systemctl is-enabled ${SVCNAM})
+
+case ${TMPSVC} in
+   masked)
+      systemctl unmask ${SVCNAM} && \
+        systemctl enable ${SVCNAM} && \
+        systemctl start ${SVCNAM} 
+      if [ $? -eq 0 ]
+      then
+         echo "/tmp mounted as tmpfs"
+         exit 0
+      else
+         echo "Failed to mount /tmp as tmpfs." > /dev/stderr
+         exit 1
+      fi
+      ;;
+   disabled)
+        systemctl enable ${SVCNAM} && \
+        systemctl start ${SVCNAM} 
+      if [ $? -eq 0 ]
+      then
+         echo "/tmp mounted as tmpfs"
+         exit 0
+      else
+         echo "Failed to mount /tmp as tmpfs." > /dev/stderr
+         exit 1
+      fi
+      ;;
+   enabled)
+      systemctl restart ${SVCNAM} 
+      if [ $? -eq 0 ]
+      then
+         echo "/tmp mounted as tmpfs"
+         exit 0
+      else
+         echo "Failed to mount /tmp as tmpfs." > /dev/stderr
+         exit 1
+      fi
+      ;;
+esac
