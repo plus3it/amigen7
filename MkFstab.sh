340c631d765a3e9a3fffcdec91131b51e9107cca
Thomas H Jones II <github@xanthia.com>
2016-03-11 00:08:47 +0000

    Start looking at lsblk as better method

--- MkFstab.sh
+++ MkFstab.sh
@@ -14,30 +14,29 @@ then
    exit 1
 fi
 
-if [[ -b $(readlink -f "${CHROOTDEV}") ]]
-then
-  PART1="$(e2label ${CHROOTDEV}2)"
-  if [[ $(file -sL "${CHROOTDEV}"2 | grep -q LVM)$? -eq 0 ]]
-  then
-     VGNAME=$(pvdisplay -c ${CHROOTDEV}2 | awk -F ":" '{print $2}')
-     echo $VGNAME
-  elif [[ $(file -sL "${CHROOTDEV}"2 | grep -q "SGI XFS")$? -eq 0 ]]
-     PART2="$(xfs_admin -l ${CHROOTDEV}2 | sed -e 's/"$//' -e 's/^.*"//')"
-  elif [[ $(file -sL "${CHROOTDEV}"2 | grep -q ext[2-4])$? -eq 0 ]]
-     PART2="$(e2label ${CHROOTDEV}2)"
-  fi
-fi   
+## # Create file-header
+## cat << EOF > ${FSTAB}
+## #
+## # /etc/fstab
+## # Created by anaconda on Mon Feb 22 17:08:22 2016
+## #
+## # Accessible filesystems, by reference, are maintained under '/dev/disk'
+## # See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
+## #
+## EOF
 
+PARTINFO=($(lsblk -nlf ${CHROOTDEV} | \
+            awk '{printf("%s:%s:%s:%s\n",$1,$2,$3,$5)}' | sed '/:$/d'))
 
-exit
-# Create file-header
-cat << EOF > ${FSTAB}
-#
-# /etc/fstab
-# Created by anaconda on Mon Feb 22 17:08:22 2016
-#
-# Accessible filesystems, by reference, are maintained under '/dev/disk'
-# See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
-#
-EOF
-
+CNT=0
+PARTLEN=${#PARTINFO[@]}
+while [[ ${CNT} -lt ${PARTLEN} ]]
+do
+   PARTNAME=$(echo "${PARTINFO[${CNT}]}" | cut -d ":" -f 1)
+   PARTTYPE=$(echo "${PARTINFO[${CNT}]}" | cut -d ":" -f 2)
+   PARTLABL=$(echo "${PARTINFO[${CNT}]}" | cut -d ":" -f 3)
+   PARTMONT=$(echo "${PARTINFO[${CNT}]}" | cut -d ":" -f 4)
+   echo "${PARTINFO[${CNT}]}"
+   echo ${PARTNAME} ${PARTTYPE} ${PARTLABL} ${PARTMONT}
+   CNT=$((${CNT} + 1))
+done
