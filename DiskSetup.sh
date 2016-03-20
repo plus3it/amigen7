b2e9b4306e69e1c9bdfb5400e18fefca4da25505
Thomas H Jones II <github@xanthia.com>
2016-03-19 20:45:06 -0400

    Script to carve up the selected chroot-target * Add rest of option-block option-flag handlers

--- DiskSetup.sh
+++ DiskSetup.sh
@@ -13,7 +13,7 @@ fi
 
 
 function ParseOpts() {
-   OPTIONBUFR=`getopt -o b:c:r:v: --longoptions chrootdev:,bootlabel:,rootlabel:,vgname: -n ${PROGNAME} -- "$@"`
+   OPTIONBUFR=`getopt -o b:d:r:v: --longoptions bootlabel:,disk:,rootlabel:,vgname: -n ${PROGNAME} -- "$@"`
    
    eval set -- "${OPTIONBUFR}"
    
@@ -32,6 +32,45 @@ function ParseOpts() {
    	          ;;
    	       *)
                   BOOTLABEL=${2}
+   	          shift 2;
+   	       ;;
+   	    esac
+   	    ;;
+         -d|--disk)
+   	    case "$2" in
+   	       "")
+   	          MultiLog "Error: option required but not specified" >&2
+   	          shift 2;
+   	          exit 1
+   	          ;;
+   	       *)
+                  CHROOTDEV=${2}
+   	          shift 2;
+   	       ;;
+   	    esac
+   	    ;;
+         -r|--rootlabel)
+   	    case "$2" in
+   	       "")
+   	          MultiLog "Error: option required but not specified" >&2
+   	          shift 2;
+   	          exit 1
+   	          ;;
+   	       *)
+                  ROOTLABEL=${2}
+   	          shift 2;
+   	       ;;
+   	    esac
+   	    ;;
+         -v|--vgname)
+   	    case "$2" in
+   	       "")
+   	          MultiLog "Error: option required but not specified" >&2
+   	          shift 2;
+   	          exit 1
+   	          ;;
+   	       *)
+                  VGNAME=${2}
    	          shift 2;
    	       ;;
    	    esac
