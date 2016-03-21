fc9b83ef984ac292be6654e36b13eacfe7b15ed5
Thomas H Jones II <github@xanthia.com>
2016-03-21 16:33:40 +0000

    Unmount chroot dev

---
+++ Umount.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Script to clean up all devices mounted under $CHROOT
+#
+#################################################################
+
+for BLK in $(mount | grep ${CHROOT} | awk '{ print $3 }' | sort -r)
+do
+   umount $BLK
+done
