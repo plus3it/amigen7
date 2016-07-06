d03c61a0597a87ff1d8fbf5487812af537196978
Thomas H Jones II <github@xanthia.com>
2016-07-06 20:13:51 +0000

    Make more resilient against LVM-enabled source-AMI

--- MkChrootTree.sh
+++ MkChrootTree.sh
@@ -123,6 +123,7 @@ BINDSOURCES=$(grep -v $CHROOT /proc/mounts | sed '{
                  /rootfs/d
                  /dev\/xvd/d
                  /\/user\//d
+                 /\/mapper\//d
               }' | awk '{print $2}')
 
 for MOUNT in ${BINDSOURCES}
@@ -134,46 +135,3 @@ do
    fi
    mount -o bind ${MOUNT} ${CHROOT}${MOUNT}
 done
-
-## # Ensure /tmp is mounted as tmpfs
-## SVCNAM="tmp.mount"
-## TMPSVC=$(systemctl is-enabled ${SVCNAM})
-## 
-## case ${TMPSVC} in
-##    masked)
-##       systemctl unmask ${SVCNAM} && \
-##         systemctl enable ${SVCNAM} && \
-##         systemctl start ${SVCNAM} 
-##       if [ $? -eq 0 ]
-##       then
-##          echo "/tmp mounted as tmpfs"
-##          exit 0
-##       else
-##          echo "Failed to mount /tmp as tmpfs." > /dev/stderr
-##          exit 1
-##       fi
-##       ;;
-##    disabled)
-##         systemctl enable ${SVCNAM} && \
-##         systemctl start ${SVCNAM} 
-##       if [ $? -eq 0 ]
-##       then
-##          echo "/tmp mounted as tmpfs"
-##          exit 0
-##       else
-##          echo "Failed to mount /tmp as tmpfs." > /dev/stderr
-##          exit 1
-##       fi
-##       ;;
-##    enabled)
-##       systemctl restart ${SVCNAM} 
-##       if [ $? -eq 0 ]
-##       then
-##          echo "/tmp mounted as tmpfs"
-##          exit 0
-##       else
-##          echo "Failed to mount /tmp as tmpfs." > /dev/stderr
-##          exit 1
-##       fi
-##       ;;
-## esac
