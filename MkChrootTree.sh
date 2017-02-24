4811240fddeb92087fc6432a0ce86507f3712472
Loren Gordon <loren@fleet-it.com>
2017-02-24 16:12:02 -0500

    Forces use of the altroot defined in the MkChrootTree script

--- MkChrootTree.sh
+++ MkChrootTree.sh
@@ -39,37 +39,37 @@ VGNAME=$(lsblk -i -o NAME,TYPE ${LVMDEV} | grep -w lvm | sed 's/^ *.-//' | \
 # Mount filesystems
 if [[ ${#VGNAME} -gt 0 ]]
 then
-   
+
    # Ensure all LVM volumes are active
    vgchange -a y ${VGNAME} || err_out 2 "Failed to activate LVM"
-   
+
    # Mount chroot base device
    echo "Mounting /dev/${VGNAME}/rootVol to ${ALTROOT}"
    mount /dev/${VGNAME}/rootVol ${ALTROOT}/ || err_out 2 "Mount Failed"
-   
+
    # Prep for next-level mounts
    mkdir -p ${ALTROOT}/{var,opt,home,boot,etc} || err_out 3 "Mountpoint Create Failed"
-   
+
    # Mount the boot-root
    echo "Mounting ${BOOTDEV} to ${ALTROOT}/boot"
    mount ${BOOTDEV} ${ALTROOT}/boot/ || err_out 2 "Mount Failed"
-   
+
    # Mount first of /var hierarchy
    echo "Mounting /dev/${VGNAME}/varVol to ${ALTROOT}/var"
    mount /dev/${VGNAME}/varVol ${ALTROOT}/var/ || err_out 2 "Mount Failed"
-   
+
    # Prep next-level mountpoints
    mkdir -p ${ALTROOT}/var/{cache,log,lock,lib/{,rpm},tmp}
-   
+
    # Mount log volume
    echo "Mounting /dev/${VGNAME}/logVol to ${ALTROOT}/var/log"
    mount /dev/${VGNAME}/logVol ${ALTROOT}/var/log
-   
+
    # Mount audit volume
    mkdir ${ALTROOT}/var/log/audit
    echo "Mounting /dev/${VGNAME}/auditVol to ${ALTROOT}/var/log/audit"
    mount /dev/${VGNAME}/auditVol ${ALTROOT}/var/log/audit
-   
+
    # Mount the rest
    echo "Mounting /dev/${VGNAME}/homeVol to ${ALTROOT}/home"
    mount /dev/${VGNAME}/homeVol ${ALTROOT}/home/
@@ -77,8 +77,8 @@ else
    ########################################################
    ## NOTE: This section assumes a simple, two-partition ##
    ##       disk with "/boot" on primary-partition 1 and ##
-   ##       "/" on primary-partition 2. This script also ## 
-   ##       assumes that each partition is labeled.      ## 
+   ##       "/" on primary-partition 2. This script also ##
+   ##       assumes that each partition is labeled.      ##
    ########################################################
 
    MNTPTS=(/boot /)
@@ -88,20 +88,20 @@ else
    # Iterate partitions and mount
    for (( IDX=${#PARTS[@]}-1 ; IDX>=0 ; IDX-- ))
    do
-      # Get partition-label 
+      # Get partition-label
       LABEL=$(e2label /dev/${PARTS[IDX]})
 
       # Ensure mount-point exists
-      if [[ ! -d ${CHROOT}${MNTPTS[IDX]} ]]
+      if [[ ! -d ${ALTROOT}${MNTPTS[IDX]} ]]
       then
-         mkdir -p ${CHROOT}${MNTPTS[IDX]}
+         mkdir -p ${ALTROOT}${MNTPTS[IDX]}
       fi
 
       # Mount partition by label
-      mount LABEL=${LABEL} ${CHROOT}${MNTPTS[IDX]}
+      mount LABEL=${LABEL} ${ALTROOT}${MNTPTS[IDX]}
    done
 
-fi    
+fi
 
 
 # Prep for loopback mounts
@@ -115,11 +115,11 @@ mknod -m 666 ${ALTROOT}/dev/random c 1 8
 mknod -m 666 ${ALTROOT}/dev/urandom c 1 9
 mknod -m 666 ${ALTROOT}/dev/tty c 5 0
 mknod -m 666 ${ALTROOT}/dev/ptmx c 5 2
-chown root:tty ${ALTROOT}/dev/ptmx 
+chown root:tty ${ALTROOT}/dev/ptmx
 
 
 # Bind-mount everything else
-BINDSOURCES=( $(grep -v $CHROOT /proc/mounts | sed '{
+BINDSOURCES=( $(grep -v $ALTROOT /proc/mounts | sed '{
                  /rootfs/d
                  /dev\/xvd/d
                  /\/user\//d
@@ -129,10 +129,10 @@ BINDSOURCES=( $(grep -v $CHROOT /proc/mounts | sed '{
 
 for MOUNT in ${BINDSOURCES[@]}
 do
-   if [[ ! -d ${CHROOT}${MOUNT} ]]
+   if [[ ! -d ${ALTROOT}${MOUNT} ]]
    then
-      mkdir -p ${CHROOT}${MOUNT} && \
-         echo "Creating ${CHROOT}${MOUNT}" || break
+      mkdir -p ${ALTROOT}${MOUNT} && \
+         echo "Creating ${ALTROOT}${MOUNT}" || break
    fi
-   mount -o bind ${MOUNT} ${CHROOT}${MOUNT}
+   mount -o bind ${MOUNT} ${ALTROOT}${MOUNT}
 done
