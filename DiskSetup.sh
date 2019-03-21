e55a82d8bf20f83ecb574db0d4739e7e1a8b3d5e
Thomas H Jones II <github@xanthia.com>
2019-03-21 12:06:17 +0000

    Convert <TAB>-characters to spaces

--- DiskSetup.sh
+++ DiskSetup.sh
@@ -115,7 +115,7 @@ function CarveBare() {
 ######################
 ## Main program-flow
 ######################
-OPTIONBUFR=$(getopt -o b:d:r:v: --long bootlabel:,disk:,rootlabel:,vgname: -n "${PROGNAME}" -- "$@")
+OPTIONBUFR=$(getopt -o b:d:f:r:v: --long bootlabel:,disk:,fstype:,rootlabel:,vgname: -n "${PROGNAME}" -- "$@")
 
 eval set -- "${OPTIONBUFR}"
 
@@ -126,57 +126,57 @@ while true
 do
    case "$1" in
       -b|--bootlabel)
-	    case "$2" in
-	       "")
-	          LogBrk 1 "Error: option required but not specified"
-	          shift 2;
-	          exit 1
-	          ;;
-	       *)
+            case "$2" in
+               "")
+                  LogBrk 1 "Error: option required but not specified"
+                  shift 2;
+                  exit 1
+                  ;;
+               *)
                BOOTLABEL=${2}
-	          shift 2;
-	       ;;
-	    esac
-	    ;;
+                  shift 2;
+               ;;
+            esac
+            ;;
       -d|--disk)
-	    case "$2" in
-	       "")
-	          LogBrk 1 "Error: option required but not specified"
-	          shift 2;
-	          exit 1
-	          ;;
-	       *)
+            case "$2" in
+               "")
+                  LogBrk 1 "Error: option required but not specified"
+                  shift 2;
+                  exit 1
+                  ;;
+               *)
                CHROOTDEV=${2}
-	          shift 2;
-	       ;;
-	    esac
-	    ;;
+                  shift 2;
+               ;;
+            esac
+            ;;
       -r|--rootlabel)
-	    case "$2" in
-	       "")
-	          LogBrk 1"Error: option required but not specified"
-	          shift 2;
-	          exit 1
-	          ;;
-	       *)
+            case "$2" in
+               "")
+                  LogBrk 1"Error: option required but not specified"
+                  shift 2;
+                  exit 1
+                  ;;
+               *)
                ROOTLABEL=${2}
-	          shift 2;
-	       ;;
-	    esac
-	    ;;
+                  shift 2;
+               ;;
+            esac
+            ;;
       -v|--vgname)
-	    case "$2" in
-	       "")
-	          LogBrk 1 "Error: option required but not specified"
-	          shift 2;
-	          exit 1
-	          ;;
-	       *)
+            case "$2" in
+               "")
+                  LogBrk 1 "Error: option required but not specified"
+                  shift 2;
+                  exit 1
+                  ;;
+               *)
                VGNAME=${2}
-	          shift 2;
-	       ;;
-	    esac
-	    ;;
+                  shift 2;
+               ;;
+            esac
+            ;;
       --)
          shift
          break
