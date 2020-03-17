cbfe535163be9e36a8de509947647b4c9f971768
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2020-03-17 18:58:48 +0000

    CleanChroot.sh: MAINTUSR could be configured by environment variable

--- CleanChroot.sh
+++ CleanChroot.sh
@@ -7,7 +7,7 @@
 CHROOT=${CHROOT:-/mnt/ec2-root}
 CLOUDCFG="$CHROOT/etc/cloud/cloud.cfg"
 JRNLCNF="$CHROOT/etc/systemd/journald.conf"
-MAINTUSR="maintuser"
+MAINTUSR=${MAINTUSR:-"maintuser"}
 
 # Disable EPEL repos
 chroot "${CHROOT}" yum-config-manager --disable "*epel*" > /dev/null
@@ -56,7 +56,7 @@ sed -i '/^system_info/,/^  ssh_svcname/d' "${CLOUDCFG}"
 sed -i '/syntax=yaml/i\
 system_info:\
   default_user:\
-    name: maintuser\
+    name: '"${MAINTUSR}"'\
     lock_passwd: true\
     gecos: Local Maintenance User\
     groups: [wheel, adm]\
