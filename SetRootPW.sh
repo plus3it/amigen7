7e36ce58875bccb6aa7ff462361fb3d89e1896f9
Thomas H Jones II <github@xanthia.com>
2020-09-28 21:21:02 +0000

    Enable remote login to launched VMs

---
+++ SetRootPW.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+# set -euo pipefail
+#
+# shellcheck disable=SC2015
+#
+# Simple script to enable maintenance-login before sealing up the template
+#
+######################################################################
+PROGNAME="$( basename "${0}" )"
+CHROOT="${CHROOT:-/mnt/ec2-root}"
+ROOTPWSTRING="${PWSTRING:-UNDEF}"
+MAINTUSER="${MAINTUSER:-root}"
+
+
+# Error handler function
+function err_exit {
+   local ERRSTR
+   local ISNUM
+   local SCRIPTEXIT
+
+   ERRSTR="${1}"
+   ISNUM='^[0-9]+$'
+   SCRIPTEXIT="${2:-1}"
+
+   if [[ ${DEBUG} == true ]]
+   then
+      # Our output channels
+      logger -i -t "${PROGNAME}" -p kern.crit -s -- "${ERRSTR}"
+   else
+      logger -i -t "${PROGNAME}" -p kern.crit -- "${ERRSTR}"
+   fi
+
+   # Only exit if requested exit is numerical
+   if [[ ${SCRIPTEXIT} =~ ${ISNUM} ]]
+   then
+      exit "${SCRIPTEXIT}"
+   fi
+}
+
+# Print out a basic usage message
+function UsageMsg {
+   local SCRIPTEXIT
+   local PART
+   SCRIPTEXIT="${1:-1}"
+
+   (
+      echo "Usage: ${0} [GNU long option] [option] ..."
+      echo "  Options:"
+      printf '\t%-4s%s\n' '-h' 'Print this message'
+      printf '\t%-4s%s\n' '-m' 'Template maintenance user (default: "root")'
+      printf '\t%-4s%s\n' '-p' 'Password to assign to template maintenance user'
+      printf '\t%-6s%s\n' '' 'Default layout:'
+      for PART in ${DEFGEOMARR[*]}
+      do
+         printf '\t%-8s%s\n' '' "${PART}"
+      done
+      echo "  GNU long options:"
+      printf '\t%-20s%s\n' '--help' 'See "-h" short-option'
+      printf '\t%-20s%s\n' '--maintuser' 'See "-m" short-option'
+      printf '\t%-20s%s\n' '--password' 'See "-p" short-option'
+   )
+   exit "${SCRIPTEXIT}"
+}
+
+function SetPassString {
+   printf "Setting password for %... " "${MAINTUSER}"
+   echo "${ROOTPWSTRING}" | chroot "${CHROOT}" /bin/passwd --stdin "${MAINTUSER}" && \
+     echo "Success" || err_exit "Failed setting password for ${MAINTUSER}" 1
+
+}
+
+function AllowRootSsh {
+   local SSHDCFGFILE
+   local CFGITEM
+
+   SSHDCFGFILE="/etc/ssh/sshd_config"
+   CFGITEM="PermitRootLogin"
+
+   printf "Allow remote-login for root... "
+   if [[ $( grep -q "^${CFGITEM}" "${SSHDCFGFILE}" )$? -eq 0 ]]
+   then
+      sed "/^${CFGITEM}/s/[ 	][ 	]*.*$/ yes/" "${SSHDCFGFILE}" && \
+        err_exit "Change ${CFGITEM} value in ${SSHDCFGFILE}" 0 || \
+        err_exit "Failed changing ${CFGITEM} value in ${SSHDCFGFILE}" 1
+   else
+      echo "PermitRootLogin yes" > "${SSHDCFGFILE}" && \
+        err_exit "Added ${CFGITEM} to ${SSHDCFGFILE}" 0 || \
+        err_exit "Failed adding ${CFGITEM} to ${SSHDCFGFILE}" 1
+   fi
+}
+
+
+
+######################
+## Main program-flow
+######################
+OPTIONBUFR=$(getopt -o hm:p: --long help,maintuser:,password: -n "${PROGNAME}" -- "$@")
+
+eval set -- "${OPTIONBUFR}"
+
+###################################
+# Parse contents of ${OPTIONBUFR}
+###################################
+while true
+do
+   case "$1" in
+      -h|--help)
+         UsageMsg 0
+         ;;
+      -m|--maintuser)
+         case "$2" in
+            "")
+               err_exit "Error: option required but not specified" 1
+               shift 2;
+               exit 1
+               ;;
+            *)
+               MAINTUSER=${2}
+               shift 2;
+               ;;
+         esac
+         ;;
+      -p|--password)
+         case "$2" in
+            "")
+               err_exit "Error: option required but not specified" 1
+               shift 2;
+               exit 1
+               ;;
+            *)
+               ROOTPWSTRING=${2}
+               shift 2;
+               ;;
+         esac
+         ;;
+      --)
+         shift
+         break
+         ;;
+      *)
+         err_exit "Internal error!"
+         exit 1
+         ;;
+   esac
+done
+
+
+# Exit if password not passed
+if [[ ${ROOTPWSTRING} == UNDEF ]]
+then
+   err_exit "No password string passed to script. ABORTING!" 1
+else
+   SetPassString
+fi
+
+# Update sshd_config as necessary
+if [[ ${MAINTUSER} == root ]]
+then
+   AllowRootSsh
+fi
